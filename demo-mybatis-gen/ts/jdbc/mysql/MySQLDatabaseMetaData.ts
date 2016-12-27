e09254f94f417ee68d2153bdca7dc303b2034f91
webdevelop <webdevelop@sohu.com>
2016-12-27 23:45:05 +0800

    查询表列

--- demo-mybatis-gen/ts/jdbc/mysql/MySQLDatabaseMetaData.ts
+++ demo-mybatis-gen/ts/jdbc/mysql/MySQLDatabaseMetaData.ts
@@ -15,6 +15,28 @@ class MySQLDatabaseMetaData implements DatabaseMetaData {
             });
         });
     }
+
+    getTableColumns(tableSchema: string, tableName: string): Promise<Array<any>> {
+        return new Promise<Array<any>>((resolve, reject) => {
+            let sql = `
+                SELECT table_name, 
+                    column_name, 
+                    ordinal_position, 
+                    is_nullable,
+                    data_type, 
+                    character_maximum_length,
+                    numeric_precision,
+                    numeric_scale,
+                    column_key,
+                    extra
+                    FROM information_schema.COLUMNS 
+                WHERE table_schema = '${tableSchema}' AND table_name = '${tableName}' ORDER BY ordinal_position`;
+
+            this.conn.query(sql, (err, rows, filed) => {
+                resolve(rows);
+            });
+        });
+    }
 }
 
 export default  MySQLDatabaseMetaData;
